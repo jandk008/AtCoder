3fcbdeb4c70786cdacf1a794a19d4dbe8a29183a
Zhou, Zicheng <zichengz@amazon.co.jp>
2017-01-16 19:17:05 +0900

    missing number to be continue

--- src/chow/zidane/ojs/leetcode/MissingNumber.java
+++ src/chow/zidane/ojs/leetcode/MissingNumber.java
@@ -2,4 +2,29 @@ package chow.zidane.ojs.leetcode;
 
 public class MissingNumber {
 
+    public int missingNumber(int[] nums) {
+        int length = nums.length;
+        int miss = -1;
+        int tmp;
+        for (int i = 0; i < length; i++) {
+            while(nums[i] != i){
+                if (nums[i] == length) {
+                    tmp = miss;
+                    miss = nums[i];
+                    nums[i] = tmp;
+                }
+                tmp = nums[nums[i]];
+                nums[nums[i]] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+        for (int i = 0;i<length;i++){
+            if (nums[i] == -1){
+                return i;
+            }
+        }
+        
+        return length;
+    }
+
 }
--- test/chow/zidane/ojs/leetcode/MissingNumberTest.java
+++ test/chow/zidane/ojs/leetcode/MissingNumberTest.java
@@ -11,21 +11,49 @@ public class MissingNumberTest {
     private MissingNumber missingNumber = new MissingNumber();
 
     @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-    }
+    public static void setUpBeforeClass() throws Exception {}
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception {}
+
+    @Test
+    public void createInstance() {
+        assertNotNull(missingNumber);
     }
 
     @Test
-    public void newInstance() {
-        assertNotNull(missingNumber);
+    public void isSingleNumber1() throws Exception {
+        int[] nums = {1};
+        int res = missingNumber.missingNumber(nums);
+        assertEquals(0, res);
+    }
+
+    @Test
+    public void isSingleNumber0() throws Exception {
+        int[] nums = {0};
+        int res = missingNumber.missingNumber(nums);
+        assertEquals(1, res);
+    }
+
+    @Test
+    public void isConsecutiveArray() throws Exception {
+        int[] nums = {0, 1};
+        int res = missingNumber.missingNumber(nums);
+        assertEquals(2, res);
+    }
+
+    @Test
+    public void missIntermediaryNumber() throws Exception {
+        int[] nums = {2,3,4,0};
+        int res = missingNumber.missingNumber(nums);
+        assertEquals(1, res);
     }
     
     @Test
-    public void sf() throws Exception {
-        
+    public void missIntermediaryNumber2() throws Exception {
+        int[] nums = {3,2,4,0};
+        int res = missingNumber.missingNumber(nums);
+        assertEquals(1, res);
     }
 
 }
