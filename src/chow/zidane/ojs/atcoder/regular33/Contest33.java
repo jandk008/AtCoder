0e8c0ab76c114e8e482587ac0b0c28fb49527127
Zhou, Zicheng <zichengz@amazon.co.jp>
2019-05-08 19:01:36 +0900

    Longest contained interval using Set
    **Problem**
    Write a program which takes as input a set of integers represented by an array, and returns the size of a largest subset of integers in the array having the property that if two integers are in the subset, then so are all integers between them. For example, if the input is (3,-2,7,9,8,1,2,0,-1,5,8), the largest such subset is{-2,-1,0,1,2,3), so you should return 6.

--- src/chow/zidane/ojs/atcoder/regular33/Contest33.java
+++ src/chow/zidane/ojs/atcoder/regular33/Contest33.java
@@ -1,102 +1,96 @@
 package chow.zidane.ojs.atcoder.regular33;
 
-import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /*
  * here lists all solutions to the questions of regular contest 33 at AtCoder
  * But question is not showed here, if you want more detail please access the home page of AtCoder
  */
-import java.util.Scanner;
-import java.util.Set;
 
 public class Contest33 {
 
-	static class A {
-		public static void main(String[] args) {
-			Scanner s = new Scanner(System.in);
-			int a = s.nextInt();
-			System.out.println(A.a(a));
-			s.close();
-		}
+    static class A {
 
-		public static int a(int a) {
-			if (a == 1)
-				return 1;
-			return a + a(a - 1);
-		}
-	}
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+            int a = s.nextInt();
+            System.out.println(A.a(a));
+            s.close();
+        }
 
-	/*
-	 * Here is the second question
-	 */
-	static class B {
-		public static void main(String[] args) {
-			Scanner in = new Scanner(System.in);
-			int na = in.nextInt();
-			int nb = in.nextInt();
-			double sumofinter = 0;
-			Set<Integer> sum = new HashSet<Integer>();
-			for (int n = 0; n < na + nb; n++) {
-				int temp = in.nextInt();
-				if (sum.contains(temp)) {
-					sumofinter++;
-				} else {
-					sum.add(temp);
-				}
-			}
-			System.out.printf("%.10f", sumofinter / sum.size());
-			in.close();
-		}
-	}
+        public static int a(int a) {
+            if (a == 1) {
+                return 1;
+            }
+            return a + a(a - 1);
+        }
+    }
 
-	/*
-	 * Here is the third question
-	 */
+    /*
+     * Here is the second question
+     */
+    static class B {
 
-	static class C {
-		public static void main(String[] args) {
-			Scanner in = new Scanner(System.in);
-			int query = in.nextInt();
-			int[] a = new int[query];
-			for (int n = 0; n < query; n++) {
-				int c = in.nextInt();
-				int v = 0;
-				if (c == 1) {
-					v = in.nextInt();
-					insert(a, v);
-				} else {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int na = in.nextInt();
+            int nb = in.nextInt();
+            double sumofinter = 0;
+            Set<Integer> sum = new HashSet<>();
+            for (int n = 0; n < na + nb; n++) {
+                int temp = in.nextInt();
+                if (sum.contains(temp)) {
+                    sumofinter++;
+                } else {
+                    sum.add(temp);
+                }
+            }
+            System.out.printf("%.10f", sumofinter / sum.size());
+            in.close();
+        }
+    }
 
-				}
-			}
+    /*
+     * Here is the third question
+     */
 
-			in.close();
-		}
+    static class C {
 
-		public static void insert(int[] a, int value) {
-			for (int n = 0; n < a.length; n++) {
-				if (a[n] == 0) {
-					a[n] = value;
-					return;
-				}
-				if (a[n] < value) {
-					continue;
-				} else {
-					int pos = n;
-					while (a[pos] != 0) {
-						int temp = a[pos + 1];
-						a[pos] = temp;
-						pos++;
-					}
-					a[n] = value;
-				}
-			}
-		}
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int query = in.nextInt();
+            int[] a = new int[query];
+            for (int n = 0; n < query; n++) {
+                int c = in.nextInt();
+                int v;
+                if (c == 1) {
+                    v = in.nextInt();
+                    insert(a, v);
+                }
+            }
 
-		public static void display(ArrayList<Integer> a) {
-			for (int n : a) {
-				System.out.println(n);
-			}
-		}
-	}
+            in.close();
+        }
+
+        static void insert(int[] a, int value) {
+            for (int n = 0; n < a.length; n++) {
+                if (a[n] == 0) {
+                    a[n] = value;
+                    return;
+                }
+                if (a[n] >= value) {
+                    int pos = n;
+                    while (a[pos] != 0) {
+                        int temp = a[pos + 1];
+                        a[pos] = temp;
+                        pos++;
+                    }
+                    a[n] = value;
+                }
+            }
+        }
+    }
 
 }
---
+++ src/chow/zidane/ojs/leetcode/LongestContainedInterval.java
@@ -0,0 +1,30 @@
+package chow.zidane.ojs.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class LongestContainedInterval {
+
+    static int find(final int[] input) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer i : input) {
+            set.add(i);
+        }
+        int max = 0;
+        for (Integer i : input) {
+            int length = 1;
+            int extend = i - 1;
+            while (set.contains(extend)) {
+                length ++;
+                set.remove(extend--);
+            }
+            extend = i + 1;
+            while (set.contains(extend)) {
+                length++;
+                set.remove(extend++);
+            }
+            max = Math.max(max, length);
+        }
+        return max;
+    }
+}
