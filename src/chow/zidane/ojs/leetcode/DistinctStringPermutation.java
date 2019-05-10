c1f2449e5186769bc9294bdd1184d933a23d390c
jandk008 <zczhou10@gmail.com>
2019-05-10 18:11:12 +0900

    Add a solution for computing permutation with an integer array given

--- src/chow/zidane/ojs/leetcode/DistinctStringPermutation.java
+++ src/chow/zidane/ojs/leetcode/DistinctStringPermutation.java
@@ -1,20 +1,52 @@
 package chow.zidane.ojs.leetcode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 class DistinctStringPermutation {
 
-    static List<String> recursion(final String s) {
+    static List<String> recursionWithString(final String s) {
         List<String> list = new LinkedList<>();
         findPermutation(s, "", list);
         return list;
     }
 
     private static void findPermutation(String s, String prefix, List<String> list) {
-        if (s.length() == 0) list.add(prefix);
+        if (s.length() == 0) {
+            list.add(prefix);
+        }
         for (int i = 0; i < s.length(); i++) {
             findPermutation(s.substring(0, i) + s.substring(i + 1), prefix + s.charAt(i), list);
         }
+    }
+
+    static List<List<Integer>> recursionWithIntegerArray(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        findPermutationWithArray(nums, 0, nums.length - 1, lists);
+        return lists;
+    }
+
+    private static void findPermutationWithArray(int[] nums, int i, int j, List<List<Integer>> lists) {
+        if (i == j) {
+            // performance purpose, stream is extremely slow comparing to simple for loop
+            List<Integer> list = new ArrayList<>();
+            for (Integer integer : nums) {
+                list.add(integer);
+            }
+            lists.add(list);
+        } else {
+            for (int index = i; i <= j; i++) {
+                swap(nums, i, index);
+                findPermutationWithArray(nums, index + 1, j, lists);
+                swap(nums, i, index);
+            }
+        }
+    }
+
+    private static void swap(final int[] nums, final int i, final int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
--- test/chow/zidane/ojs/leetcode/DistinctStringPermutationTest.java
+++ test/chow/zidane/ojs/leetcode/DistinctStringPermutationTest.java
@@ -3,15 +3,32 @@ package chow.zidane.ojs.leetcode;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 import chow.zidane.ojs.leetcode.converter.StringToStringArrayConverter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class DistinctStringPermutationTest {
+
     @ParameterizedTest
-    @CsvSource({"'abc', 'abc,acb,bac,bca,cab,cba'", "'ab','ab,ba'", "'a','a'"})
-    void permutation(String s, @ConvertWith(StringToStringArrayConverter.class) List<String> expect) {
-        assertIterableEquals(expect, DistinctStringPermutation.recursion(s));
+    @CsvSource( {"'abc,acb,bac,bca,cab,cba','abc'", "'ab,ba','ab'", "'a','a'"})
+    void permutation(@ConvertWith(StringToStringArrayConverter.class) List<String> expect, String s) {
+        assertIterableEquals(expect, DistinctStringPermutation.recursionWithString(s));
+    }
+
+    @Test
+    void permutationWthIntArray() {
+        int[] input = new int[] {1, 2, 3};
+        List<List<Integer>> expected = new ArrayList<>();
+        expected.add(Arrays.asList(1, 2, 3));
+        expected.add(Arrays.asList(1, 3, 2));
+        expected.add(Arrays.asList(2, 1, 3));
+        expected.add(Arrays.asList(2, 3, 1));
+        expected.add(Arrays.asList(3, 2, 1));
+        expected.add(Arrays.asList(3, 1, 2));
+        assertIterableEquals(expected, DistinctStringPermutation.recursionWithIntegerArray(input));
     }
 }
